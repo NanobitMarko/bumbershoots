3979fc0283bb456f17c5098545aa88ff820825c0
Branimir Trkeš <branimir.trkes@nanobit.co>
2018-11-16 15:50:13 +0100

    Obstacle added to game scene

---
+++ Bumbershoots/Assets/Resources/Scripts/Controllers/ObstacleController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour {
+
+    public BoxCollider2D obsCollider;
+
+	// Use this for initialization
+	void Start () {
+        obsCollider = GetComponent<BoxCollider2D>();
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        other.GetComponent<PlayerController>().AddDamage();
+        Destroy(other.gameObject);
+        Debug.Log("Uništen je player!");
+    }
+}
