18d8174630ceb95ab4ea3f1c7a432b77f67939b1
Branimir Trkeš <branimir.trkes@nanobit.co>
2018-11-16 14:06:37 +0100

    Camera follows character

---
+++ Bumbershoots/Assets/Resources/Scripts/Controllers/CameraManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = new Vector3(0f, target.position.y, -10f) + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(new Vector3(0f, transform.position.y, -10f), desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
+    
