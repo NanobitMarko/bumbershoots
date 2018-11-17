8f9fb3893942ddb32b8c8d62e893ba5c9516f718
Branimir Trkeš <branimir.trkes@nanobit.co>
2018-11-17 15:23:44 +0100

    Fixed collecting coins in coin snake

--- Bumbershoots/Assets/Resources/Scripts/Controllers/CoinController.cs
+++ Bumbershoots/Assets/Resources/Scripts/Controllers/CoinController.cs
@@ -3,10 +3,25 @@
 public class CoinController : MonoBehaviour {
 
     public int amountOfCoins;
+    private CircleCollider2D circleColl;
+    private SpriteRenderer spriteRend;
 
     private void Start() {
         if(amountOfCoins == 0){
             amountOfCoins = 5;
+        }
+        circleColl = GetComponent<CircleCollider2D>();
+        if(circleColl == null) {
+            this.gameObject.AddComponent<CircleCollider2D>();
+            circleColl = GetComponent<CircleCollider2D>();
+            circleColl.isTrigger = true;
+        }
+
+        spriteRend = GetComponent<SpriteRenderer>();
+        if (spriteRend == null)
+        {
+            this.gameObject.AddComponent<SpriteRenderer>();
+            spriteRend = GetComponent<SpriteRenderer>();
         }
     }
 
@@ -16,8 +31,11 @@ public class CoinController : MonoBehaviour {
         if (player != null)
         {
             player.AddCoins(amountOfCoins);
-            //Spremiti coin position za eventualno kasnije instanciranje particle effecta (prije nego se uništi coin)!
-            Destroy(gameObject);
+            Debug.Log("Dodano 5 coinsa!!!" + this.gameObject.name );
+            //Save coin position here for particle effect instance position
+            //Destroy(gameObject);
+            circleColl.enabled = false;
+            spriteRend.enabled = false;
         }
     }
 }
